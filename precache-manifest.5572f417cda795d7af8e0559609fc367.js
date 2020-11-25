ca5dd999f2f6dcdc6bbd237070e9303eb70a69a2
Olyarch <olymar.ramos20@gmail.com>
2020-11-25 17:17:19 -0500

    Updates

---
+++ precache-manifest.5572f417cda795d7af8e0559609fc367.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92af6b30b37c4961c6be3db0bf78e82a",
+    "url": "/club_recetas/index.html"
+  },
+  {
+    "revision": "c96d50444b0b7ab21b08",
+    "url": "/club_recetas/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "15d9afc7ecfa34ce55cb",
+    "url": "/club_recetas/static/css/main.d3725259.chunk.css"
+  },
+  {
+    "revision": "c96d50444b0b7ab21b08",
+    "url": "/club_recetas/static/js/2.31207744.chunk.js"
+  },
+  {
+    "revision": "94ae7515923e15d071d04e59221c5beb",
+    "url": "/club_recetas/static/js/2.31207744.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15d9afc7ecfa34ce55cb",
+    "url": "/club_recetas/static/js/main.9485c786.chunk.js"
+  },
+  {
+    "revision": "ed252d60c064f49175ed",
+    "url": "/club_recetas/static/js/runtime-main.df5719ea.js"
+  },
+  {
+    "revision": "2c1fd42f0c8d7612787732cc5cd50cf9",
+    "url": "/club_recetas/static/media/Logo.2c1fd42f.png"
+  },
+  {
+    "revision": "e5e71511c23c6d861233dd0c17bd0633",
+    "url": "/club_recetas/static/media/breakfast-4234067_1280.e5e71511.jpg"
+  },
+  {
+    "revision": "e38c73b5123b4f52b5111328d9c12695",
+    "url": "/club_recetas/static/media/mujer-cocinando-cocina_119653-116.e38c73b5.jpg"
+  },
+  {
+    "revision": "9e0949d06ec47a599875b26e6b628806",
+    "url": "/club_recetas/static/media/notepad-3316997_640.9e0949d0.jpg"
+  },
+  {
+    "revision": "827b2f5d0ad04134e0ff0c60714dac15",
+    "url": "/club_recetas/static/media/r1.827b2f5d.jpg"
+  },
+  {
+    "revision": "ffd2bf5e114a0a50f62731e4ea233b59",
+    "url": "/club_recetas/static/media/r10.ffd2bf5e.jpg"
+  },
+  {
+    "revision": "23c0e3a275a3fd7b7fc5e5c0d3327c85",
+    "url": "/club_recetas/static/media/r11.23c0e3a2.jpg"
+  },
+  {
+    "revision": "a80249fbc844f743c540eece54e7fad8",
+    "url": "/club_recetas/static/media/r12.a80249fb.jpg"
+  },
+  {
+    "revision": "1d4470f51c0bc538b0bec92ded34d9c4",
+    "url": "/club_recetas/static/media/r2.1d4470f5.jpg"
+  },
+  {
+    "revision": "c25ee37311156d55a635fb94ec846978",
+    "url": "/club_recetas/static/media/r3.c25ee373.jpg"
+  },
+  {
+    "revision": "e2cdc3334c5a71c7470518374c143903",
+    "url": "/club_recetas/static/media/r4.e2cdc333.jpg"
+  },
+  {
+    "revision": "8baec6f8984160c0307fede9396e5e8e",
+    "url": "/club_recetas/static/media/r5.8baec6f8.jpg"
+  },
+  {
+    "revision": "106d2a5ad60137e54df65ae036eb10ec",
+    "url": "/club_recetas/static/media/r6.106d2a5a.jpg"
+  },
+  {
+    "revision": "3de3e9790a6a9b5bf757d4da2a8a36d4",
+    "url": "/club_recetas/static/media/r7.3de3e979.jpg"
+  },
+  {
+    "revision": "c4a6f047180007dacacde68afe9ec755",
+    "url": "/club_recetas/static/media/r8.c4a6f047.jpg"
+  },
+  {
+    "revision": "a68488bafa4453379e358de2b66bf67e",
+    "url": "/club_recetas/static/media/r9.a68488ba.jpg"
+  }
+]);
